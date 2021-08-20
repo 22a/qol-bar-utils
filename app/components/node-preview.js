947d2125963dcc57a3268744a363ebfb5d4022a9
Peter Meehan <peter.meehan@intercom.io>
2021-08-20 04:20:00 +0100

    image previews

--- app/components/node-preview.js
+++ app/components/node-preview.js
@@ -1,3 +1,23 @@
 import Component from '@glimmer/component';
 
-export default class NodePreviewComponent extends Component {}
+export default class NodePreviewComponent extends Component {
+  get nodeNameHasImage() {
+    return this.args.nodeData.n.startsWith('::');
+  }
+
+  get nodeNameImageId() {
+    if (this.nodeNameHasImage) {
+      let iconId = (
+        this.args.nodeData.n.match(/\d+\.\d+|\d+\b|\d+(?=\w)/g) || []
+      )
+        .map(Number)
+        .shift();
+      let iconIdPad = iconId.toString().padStart(6, '0');
+      let iconBand = iconId - Math.floor(iconId % 1000);
+      let iconBandPad = iconBand.toString().padStart(6, '0');
+      return `${iconBandPad}/${iconIdPad}`;
+    } else {
+      return false;
+    }
+  }
+}
